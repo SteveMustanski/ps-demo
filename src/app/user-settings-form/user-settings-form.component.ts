548c1cae3ce78e99144f26766c63a338dd2beecb
SteveMustanski <steve.mustanski@gmail.com>
2021-02-07 14:46:32 -0500

    added data service

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserSettings } from "../data/user-settings";
+import { DataService } from "../data/data.service";
 
 @Component({
   selector: 'app-user-settings-form',
@@ -18,12 +19,17 @@ export class UserSettingsFormComponent implements OnInit {
   };
 
   userSettings: UserSettings = { ...this.originalUserSettings }
-  constructor() { }
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form : NgForm) {
     console.log(`in onsubmit: ${form.valid}`)
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+      result => console.log('success: ', result),
+      error => console.log('error: ', error)
+    );
   }
 }
